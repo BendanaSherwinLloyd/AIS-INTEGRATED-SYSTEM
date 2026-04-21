c3ea17e094940eb8cfd7a7e21ca49f9057854b0e
BendanaSherwinLloyd <23-16733@g.batstate-u.edu.ph>
2026-04-21 18:17:33 +0800

    Add files via upload

---
+++ Auth-System/controllers/UserController.js
@@ -0,0 +1,42 @@
+import * as UserModel from "../models/UserModel.js";
+
+export const register = async (req, res,) =>{
+    const {
+        firstName,
+        lastName,
+        dob,
+        course,
+        major,
+        address,
+        status,
+        email, 
+        password
+        } = req.body;
+
+    try{
+        const userProfile = {firstName, lastName, dob, course, major, address, status};
+        const user = await UserModel.createUser(userProfile, email, password);
+        res.status(201).json({success: true, message: user});
+    }catch(err){
+        console.log(err);
+        res.status(400).json({success: false, message: err});
+    }
+}
+
+export const login = async (req, res) =>{
+    const {email, password} = req.body;
+
+    try{
+        const token = await UserModel.login(email, password);
+        res.status(200).json({
+            success: true,
+            message: [
+                {result: "Login Successful"},
+                token
+            ]
+        });
+    }catch(err){
+        console.log(err);
+        res.status(400).json({success: false, message: err});
+    }
+}

3d057d623bacc876ab7a3f1e30b6bfc8ac3e6355
BendanaSherwinLloyd <23-16733@g.batstate-u.edu.ph>
2026-04-21 18:16:02 +0800

    Add files via upload

---
+++ adapter_layer/services/authService.js
@@ -0,0 +1,27 @@
+import * as AuthAdapter from '../adapters/authAdapter.js';
+
+export const registerStudent = async (studentProfile) => {
+    if(studentProfile.firstName === ''){
+        throw new Error('First name is required');
+    }
+    if(studentProfile.lastName === ''){
+        throw new Error('Last name is required');
+    }
+    if(studentProfile.dob === ''){
+        throw new Error('Date of birth is required');
+    }
+    if(studentProfile.course === ''){
+        throw new Error('Course is required');
+    }
+    if(studentProfile.major === ''){
+        throw new Error('Major is required');
+    }
+    if(studentProfile.address === ''){
+        throw new Error('Address is required');
+    }
+    if(studentProfile.status === ''){
+        throw new Error('Student status is required');
+    }
+
+    return await AuthAdapter.create(studentProfile);
+}
